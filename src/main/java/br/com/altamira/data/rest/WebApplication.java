89723ff256ec8865122a6f014c5ce6d96d41b309
Alessandro Holanda <alessandro.holanda@hotmail.com>
2014-10-16 10:53:29 -0300

    change service root url

--- src/main/java/br/com/altamira/data/rest/WebApplication.java
+++ src/main/java/br/com/altamira/data/rest/WebApplication.java
@@ -7,7 +7,7 @@ import javax.ws.rs.core.Application;
  *
  * @author alessandro.holanda
  */
-@ApplicationPath("/rest")
+@ApplicationPath("/")
 public class WebApplication extends Application {
 	
     /**
